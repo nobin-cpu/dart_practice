6e084f0fcd3d68ac7b6621adb025a041cc5e1e10
nobin-cpu <nnobin611@gmail.com>
2024-02-23 21:38:25 +0600

    list elements

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -24,7 +24,7 @@ void main() {
 
   //List properties
 
-  var cities = ["", "Rangpur", "Rajshahi"];
+/*  var cities = ["Dhaka", "Rangpur", "Rajshahi"];
 
   var citiesLength = cities.length;
   var citiesreversed = cities.reversed;
@@ -39,4 +39,37 @@ void main() {
   print(citieslastOne);
   print(citiesIndexed);
   print(citiesfirst);
+*/
+
+  // outputs
+
+/*  3
+(Rajshahi, Rangpur, Dhaka)
+Dhaka
+Rajshahi
+((0, Dhaka), (1, Rangpur), (2, Rajshahi))
+Dhaka*/
+
+//growable and fixed list
+
+//growable list elements
+  var cities = ["Dhaka", "Rangpur", "Rajshahi"];
+
+  cities.add("sylhet");
+  cities.addAll(["Munshiganj", "Narayangonj"]);
+  cities.insert(3, "coxsBazar");
+
+  //remove
+
+  cities.remove("Munshiganj");
+  cities.removeLast();
+  cities.removeAt(1);
+  print(cities);
+
+  //output
+  //[Dhaka, Rangpur, Rajshahi, sylhet]
+
+//non growable
+
+/* const cities = ["Dhaka", "Rangpur", "Rajshahi"];*/
 }
