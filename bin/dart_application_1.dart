ee7c3acc54dbb519047a002015f28324bd4ec13f
nobin-cpu <nnobin611@gmail.com>
2024-02-23 20:05:32 +0600

    List properties

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -14,11 +14,29 @@ void main() {
 
 // postfix
 
-  print(a++);
+/*  print(a++);
   print(a++);
   print(a++);
   //prefix
   print(++a);
   print(++a);
-  print(++a);
+  print(++a);*/
+
+  //List properties
+
+  var cities = ["", "Rangpur", "Rajshahi"];
+
+  var citiesLength = cities.length;
+  var citiesreversed = cities.reversed;
+  var citiesFirstOne = cities.first;
+  var citieslastOne = cities.last;
+  var citiesIndexed = cities.indexed;
+  var citiesfirst = cities.firstOrNull;
+
+  print(citiesLength);
+  print(citiesreversed);
+  print(citiesFirstOne);
+  print(citieslastOne);
+  print(citiesIndexed);
+  print(citiesfirst);
 }
